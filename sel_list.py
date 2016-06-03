7727a5d5c330d6c5c3b7c00be1f2634f5aa35db7
rbdavid <russell.davidson@colostate.edu>
2016-06-03 09:57:55 -0600

    changed list name in sel_list script...

--- sel_list.py
+++ sel_list.py
@@ -1,9 +1,9 @@
 
-sel_list = []
-sel_list.append(['residue','protein and not name H*'])
-sel_list.append(['backbone','backbone'])
-sel_list.append(['sidechain','protein and not backbone and not name H*'])
-sel_list.append(['C_alpha','protein and name CA'])
+sel = []
+sel.append(['residue','protein and not name H*'])
+sel.append(['backbone','backbone'])
+sel.append(['sidechain','protein and not backbone and not name H*'])
+sel.append(['C_alpha','protein and name CA'])
 
 # CHECK THAT ATP, ADP, PI, and NUCLEIC RESIDUES ARE BEING ANALYZED... IF NOT CREATE ATOM SELECTIONS TO DO SO... 
 
