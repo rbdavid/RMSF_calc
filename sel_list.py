d49918c8935edafa0a7243b0c356c3f7c4b7da72
rbdavid <russell.davidson@colostate.edu>
2016-07-06 13:25:39 -0600

    substrates are not being analyzed; need to figure this out

--- sel_list.py
+++ sel_list.py
@@ -2,9 +2,6 @@
 sel = []
 sel.append(['residue','not name H*'])
 sel.append(['backbone','backbone'])
-#sel.append(['sidechain_test','resname GLY and name ...' NOT GOING TO FUCKING WORK...)]
-
-#sel.append(['sidechain','not backbone and not (resname GLY or name H*)'])
 sel.append(['C_alpha','protein and name CA'])
 
 # CHECK THAT ATP, ADP, PI, and NUCLEIC RESIDUES ARE BEING ANALYZED... IF NOT CREATE ATOM SELECTIONS TO DO SO... 
